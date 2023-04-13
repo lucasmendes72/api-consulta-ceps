bcecce57e83ef2a3392912be178caa1b9d0dae48
Lucas Mendes <luckasmendys72@hotmail.com>
2023-04-13 11:04:50 -0300

    estilizando a página inicialmente

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
 const campoCep = document.getElementById('cep');
 const botaoEnviar = document.getElementById('envia');
+const divBuscador = document.querySelector('.buscador-conteiner');
 
 async function consultaCep(cep) {
     const consulta = await fetch(`https://viacep.com.br/ws/${cep}/json/`)
@@ -15,4 +16,12 @@ botaoEnviar.addEventListener('click', async (event) => {
     event.preventDefault();
     const resposta = await consultaCep(campoCep.value);
     console.log(resposta);
+    divBuscador.innerHTML += `<div class="cep-buscado"><p>Mostrando pesquisa para o CEP: ${campoCep.value}</p></div`
+    document.body.innerHTML +=`<div class="mostrandoDados">
+      <p>Estado: ${resposta.uf}</p><br>
+      <p>Localidade: ${resposta.localidade}</p><br>
+      <p>DDD: ${resposta.ddd}</p><br>
+      <p>Bairro: ${resposta.bairro}</p><br>
+      <p>Logradouro: ${resposta.logradouro}</p><br>
+    </div>`
 })
