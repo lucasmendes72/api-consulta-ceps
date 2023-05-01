ddb63d6c40203d0853004eee7d2c87aafa4a58eb
Lucas Mendes <luckasmendys72@hotmail.com>
2023-05-01 14:30:11 -0300

    adicionando mensagens de erros e instruções

--- main.js
+++ main.js
@@ -1,33 +1,42 @@
+async function consultaCep(cep) {
+  const consulta = await fetch(`https://viacep.com.br/ws/${cep}/json/`);
+  const resposta = await consulta.json();
+  return resposta;
+}
+
 const campoCep = document.getElementById('cep');
 const botaoEnviar = document.getElementById('envia');
-const botaoNovaBusca = document.getElementById('nova-busca');
 const divBuscador = document.querySelector('.buscador-conteiner');
 const divResult = document.querySelector('.result');
 
-async function consultaCep(cep) {
-    const consulta = await fetch(`https://viacep.com.br/ws/${cep}/json/`)
-     .then( resposta => {
-        return resposta.json()
-     }).catch (err => {
-        console.log(err);
-        document.body.innerHTML += `<div class="mostrandoDados"><p>Cep Inválido!Tente Novamente</p></div>`
-     })
-     return consulta;
-}
+const cepRegex = /[0-9]{8}/;
 
-botaoEnviar.addEventListener('click', async (event) => {
-    event.preventDefault();
-    botaoEnviar.style.display = "none";
-    botaoNovaBusca.style.display = "inline-block";
-    divResult.innerHTML = "";
-    const resposta = await consultaCep(campoCep.value);
-    console.log(resposta);
-    divBuscador.innerHTML += `<div class="cep-buscado"><p>Mostrando pesquisa para o CEP: ${campoCep.value}</p></div>`;
-    divResult.innerHTML = `<div class="mostrandoDados">
-      <p>Estado: ${resposta.uf}</p><br>
-      <p>Localidade: ${resposta.localidade}</p><br>
-      <p>DDD: ${resposta.ddd}</p><br>
-      <p>Bairro: ${resposta.bairro}</p><br>
-      <p>Logradouro: ${resposta.logradouro}</p><br>
-    </div>`
-})
+botaoEnviar.onclick = async (event) => {
+  event.preventDefault();
+  if (!cepRegex.test(campoCep.value)) {
+    campoCep.setCustomValidity('Digite um CEP válido (8 dígitos).');
+  } else {
+    campoCep.setCustomValidity('');
+    divResult.innerHTML = '';
+    divBuscador.innerHTML += `<div class="cep-buscado"><p>Mostrando pesquisa para o CEP: ${campoCep.value}</p>
+    <p>Deseja fazer uma NOVA BUSCA? Recarregue a página!</p></div>`;
+    try {
+      const resposta = await consultaCep(campoCep.value);
+      divResult.innerHTML = `<div class="mostrandoDados">
+        <p>Logradouro: ${resposta.logradouro}</p><br>
+        <p>Bairro: ${resposta.bairro}</p><br>
+        <p>Localidade: ${resposta.localidade}</p><br>
+        <p>Estado: ${resposta.uf}</p><br>
+        <p>DDD: ${resposta.ddd}</p><br>
+      </div>`;
+
+    } catch (err) {
+      console.log(err);
+      divResult.innerHTML = `<div class="mostrandoDados"><p>Cep Inválido! Tente Novamente</p></div>`;
+    }
+  }
+
+  campoCep.style.display = "none";
+  botaoEnviar.style.display = "none";
+};
+
