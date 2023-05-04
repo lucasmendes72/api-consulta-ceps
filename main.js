aa965933032cf9772fd2059cb0770bb0efd8d05e
Lucas Mendes <luckasmendys72@hotmail.com>
2023-05-04 19:52:34 -0300

    Corrigindo lógica do botão de envio e adicionando ligação entre as páginas do projeto atrávés do menu no header

--- main.js
+++ main.js
@@ -13,34 +13,22 @@ const cepRegex = /[0-9]{8}/;
 
 botaoEnviar.onclick = async (event) => {
   event.preventDefault();
-  if (!cepRegex.test(campoCep.value)) {
-    campoCep.setCustomValidity('Digite um CEP válido (8 dígitos).');
-  } else {
-    campoCep.setCustomValidity('');
-    divResult.innerHTML = '';
-    divBuscador.innerHTML += `<div class="cep-buscado"><p>Mostrando pesquisa para o CEP: ${campoCep.value}</p>
-    <p>Deseja fazer uma NOVA BUSCA? Recarregue a página!</p></div>`;
-    try {
-      const resposta = await consultaCep(campoCep.value);
-      divResult.innerHTML = `<div class="mostrandoDados">
-        <p>Logradouro: ${resposta.logradouro}</p><br>
-        <p>Bairro: ${resposta.bairro}</p><br>
-        <p>Localidade: ${resposta.localidade}</p><br>
-        <p>Estado: ${resposta.uf}</p><br>
-        <p>DDD: ${resposta.ddd}</p><br>
-      </div>`;
 
-    } catch (err) {
-      console.log(err);
-      divResult.innerHTML = `<div class="mostrandoDados"><p>Cep Inválido! Tente Novamente</p></div>`;
-    }
+  try {
+    const resposta = await consultaCep(campoCep.value);
+    divResult.innerHTML = `<div class="mostrandoDados">
+      <p>Logradouro: ${resposta.logradouro}</p><br>
+      <p>Bairro: ${resposta.bairro}</p><br>
+      <p>Localidade: ${resposta.localidade}</p><br>
+      <p>Estado: ${resposta.uf}</p><br>
+      <p>DDD: ${resposta.ddd}</p><br>
+    </div>`;
+    campoCep.value = '';
+
+  } catch (err) {
+    console.log(err);
+    divResult.innerHTML = `<div class="mostrandoDados"><p>Cep Inválido! Tente Novamente</p></div>`;
   }
-
-  campoCep.style.display = "none";
-  botaoEnviar.style.display = "none";
 };
 
-// export { consultaCep };
-
-// Código da Página de Cadastros
 
